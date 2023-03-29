77b1b98dae97ceafb382907e4a58b332e7ee4e28
Renato <renatoandre2000@hotmail.com>
2023-03-29 15:39:51 +0100

    Debbug comments

--- src/scheduling_algorithm/FifoAlgorithm.cpp
+++ src/scheduling_algorithm/FifoAlgorithm.cpp
@@ -28,9 +28,7 @@ std::vector<wrench::WorkflowTask *> FifoAlgorithm::sortTasks(const vector<wrench
  */
 std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
 
-
-    // find free vm
-
+    std::cout << "Scheduling a new task" << std::endl;
 
     // find candidate vms
     std::vector<std::string> candidate_vms;
@@ -43,22 +41,27 @@ std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
     for (const auto &vm : candidate_vms) {
         if (this->cloud_service->isVMRunning(vm) &&
             this->cloud_service->getVMComputeService(vm)->getTotalNumIdleCores() > 0) {
+            std::cout << vm << " is running and has available idle cores" << std::endl;
             return vm;
 
         } else if (vm_name.empty() && this->cloud_service->isVMDown(vm)) {
+            std::cout << vm << " is down but could be turned on to schedule tasks..." << std::endl;
             vm_name = vm;
         }
     }
 
     if (vm_name.empty()) {
+        std::cout << "Could not find a suitable vm" << std::endl;
         // create VM, as no viable VM could be found
         if (this->cloud_service->getTotalNumIdleCores() == 0) {
+            std::cout << "There are not enough resources on the cluster" << std::endl;
             return "";
         }
         bool turned_on = false;
         int num_cores = 1;
         for (auto &host : this->cloud_service->getExecutionHosts()) {
             if (!wrench::Simulation::isHostOn(host)) {
+                std::cout << host << " is turned off, so it will be turned on" << std::endl;
                 wrench::Simulation::turnOnHost(host);
                 num_cores = wrench::Simulation::getHostNumCores(host);
                 turned_on = true;
@@ -66,15 +69,18 @@ std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
             }
         }
         if (turned_on) {
+            std::cout << "Creating a new VM" << std::endl;
             vm_name = this->cloud_service->createVM(num_cores, 1000000000);
         }
     }
 
     if (vm_name.empty()) {
+        std::cout << "Could not find a VM" << std::endl;
         return "";
     }
 
     // start VM
+    std::cout << "Starting VM " << vm_name << std::endl;
     this->cloud_service->startVM(vm_name);
     auto vm_pm = this->cloud_service->getVMPhysicalHostname(vm_name);
 
