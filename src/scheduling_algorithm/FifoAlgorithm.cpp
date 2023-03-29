9e0b8847efa9e792060ec753090c5511969a99be
Renato <renatoandre2000@hotmail.com>
2023-03-29 16:08:31 +0100

    Debbug comments

--- src/scheduling_algorithm/FifoAlgorithm.cpp
+++ src/scheduling_algorithm/FifoAlgorithm.cpp
@@ -80,7 +80,7 @@ std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
     }
 
     // start VM
-    auto vm_cs = cloud_service->getVMComputeService(it->second);
+    auto vm_cs = cloud_service->getVMComputeService(vm_name);
     std::cout << "Starting VM " << vm_name << " which has " << vm_cs->getTotalNumIdleCores() << " idle cores" << std::endl;
     this->cloud_service->startVM(vm_name);
     auto vm_pm = this->cloud_service->getVMPhysicalHostname(vm_name);
