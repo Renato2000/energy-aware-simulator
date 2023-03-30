ea4baddb3ab8dbfdd8751d975e61115163c1865b
Renato <renatoandre2000@hotmail.com>
2023-03-30 15:32:26 +0100

    Debbug comments

--- src/scheduling_algorithm/FifoAlgorithm.cpp
+++ src/scheduling_algorithm/FifoAlgorithm.cpp
@@ -18,7 +18,18 @@ FifoAlgorithm::FifoAlgorithm(std::shared_ptr<wrench::CloudComputeService> &cloud
  * @return
  */
 std::vector<wrench::WorkflowTask *> FifoAlgorithm::sortTasks(const vector<wrench::WorkflowTask *> &tasks) {
-    return tasks;
+    auto sorted_tasks = tasks;
+
+    std::sort(sorted_tasks.begin(), sorted_tasks.end(),
+              [](const wrench::WorkflowTask *t1, const wrench::WorkflowTask *t2) -> bool {
+                  if (t1->getFlops() == t2->getFlops()) {
+                      return ((uintptr_t) t1 < (uintptr_t) t2);
+                  } else {
+                      return (t1->getFlops() > t2->getFlops());
+                  }
+              });
+
+    return sorted_tasks;
 }
 
 /**
@@ -41,27 +52,22 @@ std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
     for (const auto &vm : candidate_vms) {
         if (this->cloud_service->isVMRunning(vm) &&
             this->cloud_service->getVMComputeService(vm)->getTotalNumIdleCores() > 0) {
-            std::cout << vm << " is running and has available idle cores" << std::endl;
             return vm;
 
         } else if (vm_name.empty() && this->cloud_service->isVMDown(vm)) {
-            std::cout << vm << " is down but could be turned on to schedule tasks..." << std::endl;
             vm_name = vm;
         }
     }
 
     if (vm_name.empty()) {
-        std::cout << "Could not find a suitable vm" << std::endl;
         // create VM, as no viable VM could be found
         if (this->cloud_service->getTotalNumIdleCores() == 0) {
-            std::cout << "There are not enough resources on the cluster" << std::endl;
             return "";
         }
         bool turned_on = false;
         int num_cores = 1;
         for (auto &host : this->cloud_service->getExecutionHosts()) {
             if (!wrench::Simulation::isHostOn(host)) {
-                std::cout << host << " is turned off, so it will be turned on" << std::endl;
                 wrench::Simulation::turnOnHost(host);
                 num_cores = wrench::Simulation::getHostNumCores(host);
                 turned_on = true;
@@ -69,20 +75,16 @@ std::string FifoAlgorithm::scheduleTask(const wrench::WorkflowTask *task) {
             }
         }
         if (turned_on) {
-            std::cout << "Creating a new VM" << std::endl;
             vm_name = this->cloud_service->createVM(num_cores, 1000000000);
         }
     }
 
     if (vm_name.empty()) {
-        std::cout << "Could not find a VM" << std::endl;
         return "";
     }
 
     // start VM
     this->cloud_service->startVM(vm_name);
-    auto vm_cs = cloud_service->getVMComputeService(vm_name);
-    std::cout << "Starting VM " << vm_name << " which has " << vm_cs->getTotalNumIdleCores() << " idle cores" << std::endl;
     auto vm_pm = this->cloud_service->getVMPhysicalHostname(vm_name);
 
     if (this->vm_worker_map.find(vm_name) == this->vm_worker_map.end()) {
