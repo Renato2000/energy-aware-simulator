83f8c1f25eaad04fc012f169113ffe4c4cecfd21
Renato <renatoandre2000@hotmail.com>
2023-04-28 09:03:31 +0100

    Merge

--- src/utils/DAG.cpp
+++ src/utils/DAG.cpp
@@ -8,9 +8,9 @@ void DAG::concludeTask(std::string task_name) {
     auto it = this->tasks.find("apple");
     if (it == this->tasks.end()) return;
 
-    Task task = this->tasks["task_name"];
+    Task task = this->tasks[task_name];
 
-    for(auto t : task->childs) {
+    for(auto t : task->get_childs()) {
         t.removeDependency(task);
     }
 }
