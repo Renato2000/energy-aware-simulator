7325bcf2be974fe2af0327e33e89bc5e06d93533
Renato <renatoandre2000@hotmail.com>
2023-05-10 14:08:37 +0100

    Some small changes

--- src/utils/ClusterInfo.cpp
+++ src/utils/ClusterInfo.cpp
@@ -1,33 +1,11 @@
 #include "ClusterInfo.h"
 
-void ClusterInfo::run_task(std::string task, std::string executor) {
-    this->dag->complete_task(task);
-    this->executors->run_task(task, executor);
+void ClusterInfo::set_current_task_list(std::vector<wrench::WorkflowTask *> tasks) {
+    this->current_task_list = tasks;
 }
 
-void ClusterInfo::complete_task(std::string task) {
-    //this->dag->complete_task(task);
-    this->executors->complete_task(task);
-}
-
-const std::list<std::string> ClusterInfo::get_running_tasks(std::string executor) {
-    return this->executors->get_running_tasks(executor);
-}
-
-float ClusterInfo::predict_time(std::string task) {
-    return this->dag->get_expected_runtime(task);
-}
-
-void ClusterInfo::create_executor(std::string host, std::string executor) {
-    this->executors->add_executor(host, executor);
-}
-
-void ClusterInfo::remove_executor(std::string executor) {
-    this->executors->remove_executor(executor);
-}
-
-void ClusterInfo::set_current_task_list(std::vector<std::string> tasks) {
-    
+int ClusterInfo::get_number_remaining_tasks() {
+    return this->current_task_list.size();
 }
 
 bool ClusterInfo::is_priority(std::string task) {
@@ -38,13 +16,23 @@ bool ClusterInfo::is_last(std::string task) {
     return false;
 }
 
-int ClusterInfo::get_number_remaining_tasks() {
-    return 0;
-}
-
 std::list<wrench::WorkflowTask *> ClusterInfo::get_tasks(std::string vm) {
     std::list<wrench::WorkflowTask *> ret;
 
     return ret;
 }
 
+float ClusterInfo::predict_time(std::string task) {
+    return this->dag->get_expected_runtime(task);
+}
+
+void ClusterInfo::run_task(std::string task, std::string executor) {
+    this->dag->complete_task(task);
+    this->executors->run_task(task, executor);
+}
+
+
+void ClusterInfo::complete_task(std::string task) {
+    //this->dag->complete_task(task);
+    this->executors->complete_task(task);
+}
--- src/utils/ClusterInfo.h
+++ src/utils/ClusterInfo.h
@@ -14,23 +14,17 @@ class ClusterInfo {
             dag(dag),
             executors(executors) {}
 
-        void set_number_unscheduled_tasks(int num);
-        int get_number_unscheduled_tasks();
-
-        void set_current_task_list(std::vector<std::string> tasks);
+        void set_current_task_list(std::vector<wrench::WorkflowTask *> tasks);
+        int get_number_remaining_tasks();
         bool is_priority(std::string task);
         bool is_last(std::string task);
-        int get_number_remaining_tasks();
-        std::list<wrench::WorkflowTask *> get_tasks(std::string vm);
-
+        std::vector<wrench::WorkflowTask *> get_tasks(std::string vm);
+        float predict_time(std::string task);
         void run_task(std::string task, std::string executor);
         void complete_task(std::string task);
-        const std::list<std::string> get_running_tasks(std::string executor);
-        float predict_time(std::string task);
-        void create_executor(std::string host, std::string executor);
-        void remove_executor(std::string executor);
 
     private:
+        std::vector<wrench::WorkflowTask *> current_task_list;
         std::unique_ptr<DAG> &dag;
         std::unique_ptr<Executors> &executors;        
 };
